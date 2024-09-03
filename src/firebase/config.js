64c31974725a462bce2f04353fe1a82f99c149a5
CoderRocha <guilhermerochas0420@gmail.com>
2024-09-03 19:15:17 -0300

    installed 'firebase'

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA8MDnp1tnwA2CzQNYrRAqv4pKxfVHE5So",
+    authDomain: "mybread-5ba28.firebaseapp.com",
+    projectId: "mybread-5ba28",
+    storageBucket: "mybread-5ba28.appspot.com",
+    messagingSenderId: "922486569216",
+    appId: "1:922486569216:web:7d9d97e64835c47b259be5"
+  };
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig)
+
+// initialize services
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
