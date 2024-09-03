86889e8c674fdd46ca7b8450a638e97230b199e0
CoderRocha <guilhermerochas0420@gmail.com>
2024-09-03 19:22:15 -0300

    added 'firebase/auth' to the project

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import 'firebase/firestore'
+import 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: "AIzaSyA8MDnp1tnwA2CzQNYrRAqv4pKxfVHE5So",
@@ -15,5 +16,6 @@ firebase.initializeApp(firebaseConfig)
 
 // initialize services
 const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
 
-export { projectFirestore }
+export { projectFirestore, projectAuth }
