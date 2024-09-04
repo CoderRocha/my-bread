9fa44bff164ce92c8293e0732a960fdf8de7ab78
CoderRocha <guilhermerochas0420@gmail.com>
2024-09-04 10:46:15 -0300

    added project logo to the 'Navbar' component and changed the text color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,15 @@ import { Link } from 'react-router-dom'
 
 //styles
 import styles from './Navbar.module.css'
+import myBreadIcon from '../assets/my-bread-icon.png'
 
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <ul>
+        <img
+        src={myBreadIcon}
+        />
         <li className={styles.title}>myBread</li>
 
         <li><Link to={"/login"}>Login</Link></li>
