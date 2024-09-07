1deaf01f19b8ed017e42f38cce5bde703157a4dd
CoderRocha <guilhermerochas0420@gmail.com>
2024-09-07 12:05:05 -0300

    added 'signup' hook to the 'signup' component

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import { useSignup } from '../../hooks/useSignup'
 
 //styles
 import styles from './Signup.module.css'
@@ -8,15 +9,16 @@ export default function Signup() {
   const [displayName, setDisplayName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const { signup, isPending, error } = useSignup()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(displayName, email, password)
+    signup(displayName, email, password)
   }
 
   return (
     <form onSubmit={handleSubmit} className={styles['signup-form']}>
-      <h2>Signup</h2>
+      <h2>Sign up</h2>
       <label>
         <span>Name:</span>
         <input
@@ -41,7 +43,9 @@ export default function Signup() {
         value={password}
         />
       </label>
-      <button className="btn">Signup!</button>
+      {!isPending && <button className="btn">Sign up!</button>}
+      {isPending && <button className="btn" disabled>Loading...</button>}
+      {error && <p>{error}</p> }
     </form>
   )
 }
