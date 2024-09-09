5cc760b5f39b6d112323c992aa5a63ce142b265d
CoderRocha <guilhermerochas0420@gmail.com>
2024-09-09 10:25:37 -0300

    added 'logout' hook function to the application

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 //styles
 import styles from './Navbar.module.css'
 import myBreadIcon from '../assets/my-bread-icon.png'
 
 export default function Navbar() {
+  const { logout } = useLogout()
+
   return (
     <nav className={styles.navbar}>
       <ul>
@@ -15,7 +18,11 @@ export default function Navbar() {
         <li className={styles.title}>myBread</li>
 
         <li><Link to={"/login"}>Login</Link></li>
-        <li><Link to={"/signup"}>Signup</Link></li>
+        <li><Link to={"/signup"}>Sign up</Link></li>
+
+        <li>
+          <button className="btn" onClick={logout}>Log out</button>
+        </li>
       </ul>
     </nav>
   )
