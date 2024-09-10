8763ba680a2f39b14a6099eddde83a173b381aee
CoderRocha <guilhermerochas0420@gmail.com>
2024-09-10 18:38:48 -0300

    added button to delete transactions

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import { useFirestore } from '../../hooks/useFirestore'
 
 // styles
 import Styles from './Home.module.css'
 
 export default function TransactionList({ transactions }) {
+  const { deleteDocument, response } = useFirestore('transactions')
+  console.log(response)
+
   return (
     <ul className={Styles.transactions}>
         {transactions.map((transaction) => (
             <li key={transaction.id}>
                 <p className={Styles.name}>{transaction.name}</p>
                 <p className={Styles.amount}>${transaction.amount}</p>
+                <button onClick={() => deleteDocument(transaction.id) } className={Styles.button}>X</button>
             </li>
         ))}
     </ul>
